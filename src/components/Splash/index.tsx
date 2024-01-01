1c4db47149827e966cdfba7bad4d506a7a5ba6e3
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-01-01 21:53:19 +0900

    Feat: Splash 와 로그인 초기화면 추가

---
+++ src/components/Splash/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./Splash.module.css";
+
+import zipkokIcon from "../../assets/zipkokIcon_red.svg";
+
+export default function Splash() {
+  return (
+    <div className={styles.root}>
+      <img className={styles.zipkokIcon} src={zipkokIcon}></img>
+      <div className={styles.Label}>내가 살 자취방을 ‘콕’하다</div>
+    </div>
+  );
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,40 @@
-import React, { useState } from "react";
-
-import styles from "./Login.module.scss";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import kakaoIcon from "../../assets/kakaoIcon.svg";
+import zipkokIcon from "../../assets/zipkokIcon_white.svg";
+import Splash from "../../components/Splash";
+import styles from "./Login.module.css";
 
 export default function Login() {
-    return <div>Login</div>;
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className={styles.root}>
+      {showSplash ? (
+        <Splash />
+      ) : (
+        <>
+          <img
+            className={styles.zipkokIcon}
+            src={zipkokIcon}
+            alt="zipkokIcon"
+          />
+          <Link to="/sign">
+            <button className={styles.loginBtn}>
+              <img src={kakaoIcon} alt="kakaoIcon" />
+              <div>카카오 로그인</div>
+            </button>
+          </Link>
+          <Link to="/">
+            <button className={styles.notLoginBtn}>로그인 없이 둘러보기</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
