e8049c97e8b7b4dbba1bfb4f26846eb03984d0f6
parkcoool <yickle1345@naver.com>
2024-01-01 18:06:54 +0900

    Fix: 주소 검색 결과

--- src/pages/Onboarding/Location/index.tsx
+++ src/pages/Onboarding/Location/index.tsx
@@ -136,12 +136,6 @@ export default function Location({ confirmLocation }: LocationProps) {
                 onClick={handleAddressClick}
                 onEndOfScroll={loadMoreAddresses}
             />
-            <AddressContainer
-                errorMessage={errorMessage}
-                addresses={addresses}
-                onClick={handleAddressClick}
-                onEndOfScroll={loadMoreAddresses}
-            />
             {addresses.length === 0 && (
                 <BottomBtn
                     onClick={handleSubmit}
