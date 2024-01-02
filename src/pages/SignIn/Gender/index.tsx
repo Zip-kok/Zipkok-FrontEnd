2ff2d5638fde045f72706fc542cd63957f01399e
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-01-02 17:22:45 +0900

    Feat: 성별 페이지 추가

--- src/pages/SignIn/Gender/index.tsx
+++ src/pages/SignIn/Gender/index.tsx
@@ -1,6 +1,25 @@
+import React, { useState } from "react";
+import BottomBtn from "../../../components/BottomBtn";
+import RadioBtn from "../../../components/RadioBtn";
 import styles from "./Gender.module.css";
-export default function Gender() {
-  function handleSubmit() {}
+
+interface GenderProps {
+  onConfirm: () => void;
+}
+
+export default function Gender({ onConfirm }: GenderProps) {
+  const genderOptions = ["남성", "여성", "비공개"];
+  const [selectedGenderIndex, setSelectedGenderIndex] = useState(-1);
+
+  const handleGenderSelect = (index: number) => {
+    setSelectedGenderIndex(index);
+  };
+
+  const handleSubmit = () => {
+    if (selectedGenderIndex !== -1) {
+      onConfirm();
+    }
+  };
 
   return (
     <div className={styles.root}>
@@ -10,6 +29,25 @@ export default function Gender() {
           <br />
           알려주세요.
         </h1>
+      </div>
+
+      <ul>
+        {genderOptions.map((gender, index) => (
+          <RadioBtn
+            key={index}
+            content={gender}
+            isSelected={selectedGenderIndex === index}
+            handleClick={() => handleGenderSelect(index)}
+          />
+        ))}
+      </ul>
+
+      <div className={styles.wrapper}>
+        <BottomBtn
+          onClick={handleSubmit}
+          text="확인"
+          disabled={selectedGenderIndex === -1}
+        />
       </div>
     </div>
   );
