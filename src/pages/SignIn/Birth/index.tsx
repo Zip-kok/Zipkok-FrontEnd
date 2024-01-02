888906a120b5c8784f613a9a05a1cacd520ba725
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-01-02 17:22:00 +0900

    Feat: 생년월일 페이지 추가

--- src/pages/SignIn/Birth/index.tsx
+++ src/pages/SignIn/Birth/index.tsx
@@ -1,3 +1,56 @@
-export default function Birth() {
-  return <div>Birth</div>;
+import React, { FormEvent, useState } from "react";
+import BottomBtn from "../../../components/BottomBtn";
+import LoginInput from "../../../components/LoginInput";
+import styles from "./Birth.module.css";
+
+interface BirthProps {
+  onConfirm: () => void;
 }
+
+const Birth: React.FC<BirthProps> = ({ onConfirm }) => {
+  const [birth, setBirth] = useState<string>("");
+  const [isBirthValid, setIsBirthValid] = useState<boolean>(false);
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    const target = e.currentTarget;
+    const value = target.value;
+    const isValid = value.length === 6 && /^\d+$/.test(value);
+
+    setBirth(value);
+    setIsBirthValid(isValid);
+  };
+
+  const handleSubmit = () => {
+    if (isBirthValid) {
+      onConfirm();
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <h1>
+          생년월일을
+          <br />
+          알려주세요.
+        </h1>
+      </div>
+      <LoginInput
+        value={birth}
+        placeholder="6자리 숫자로 입력해주세요"
+        numberOnly
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <div className={styles.wrapper}>
+        <BottomBtn
+          onClick={handleSubmit}
+          text="확인"
+          disabled={!isBirthValid}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Birth;
