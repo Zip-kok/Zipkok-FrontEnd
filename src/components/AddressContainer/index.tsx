be3ad5d4b770a559057174ff8b4c16a6914683cc
parkcoool <yickle1345@naver.com>
2024-01-09 15:36:48 +0900

    Style: 코드 스타일 통일

--- src/components/AddressContainer/index.tsx
+++ src/components/AddressContainer/index.tsx
@@ -1,32 +1,41 @@
-import React from "react";
-import styles from "./AddressContainer.module.css";
-import Address from "../../types/Address";
-import AddressBtn from "../AddressBtn";
+import React from 'react';
+import styles from './AddressContainer.module.css';
+import Address from '../../types/Address';
+import AddressBtn from '../AddressBtn';
 
 interface AddressContainerProps {
-    errorMessage?: string;
-    addresses: Address[];
-    onClick: (address: Address) => void;
-    onEndOfScroll?: () => void;
+  errorMessage?: string;
+  addresses: Address[];
+  onClick: (address: Address) => void;
+  onEndOfScroll?: () => void;
 }
 
-export default function AddressContainer({ errorMessage, addresses, onClick, onEndOfScroll }: AddressContainerProps) {
-    function handleScroll(e: React.UIEvent<HTMLDivElement, UIEvent>) {
-        const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
-        if (Math.abs(scrollHeight - scrollTop - clientHeight) <= 10) {
-            if (onEndOfScroll) onEndOfScroll();
-        }
+export default function AddressContainer({
+  errorMessage,
+  addresses,
+  onClick,
+  onEndOfScroll,
+}: AddressContainerProps) {
+  function handleScroll(e: React.UIEvent<HTMLDivElement, UIEvent>) {
+    const { scrollTop, scrollHeight, clientHeight } = e.currentTarget;
+    if (Math.abs(scrollHeight - scrollTop - clientHeight) <= 10) {
+      if (onEndOfScroll) onEndOfScroll();
     }
+  }
 
-    return (
-        <div className={styles.container} onScroll={handleScroll}>
-            {errorMessage !== "" ? (
-                <span className={styles.errorMessage}>{errorMessage}</span>
-            ) : (
-                addresses.map((address) => (
-                    <AddressBtn key={address.bdMgtSn} address={address} onClick={() => onClick(address)} />
-                ))
-            )}
-        </div>
-    );
+  return (
+    <div className={styles.container} onScroll={handleScroll}>
+      {errorMessage !== '' ? (
+        <span className={styles.errorMessage}>{errorMessage}</span>
+      ) : (
+        addresses.map((address) => (
+          <AddressBtn
+            key={address.bdMgtSn}
+            address={address}
+            onClick={() => onClick(address)}
+          />
+        ))
+      )}
+    </div>
+  );
 }
--- src/components/Btn/index.tsx
+++ src/components/Btn/index.tsx
@@ -1,21 +1,26 @@
-import React from "react";
-import styles from "./Btn.module.css";
+import React from 'react';
+import styles from './Btn.module.css';
 
 interface BtnProps {
-    onClick?: () => void;
-    text: string;
-    isKeyboardOpen?: boolean;
-    disabled?: boolean;
+  onClick?: () => void;
+  text: string;
+  isKeyboardOpen?: boolean;
+  disabled?: boolean;
 }
 
-export default function Btn({ onClick, text, isKeyboardOpen, disabled }: BtnProps) {
-    return (
-        <button
-            className={`${styles.btn} ${!isKeyboardOpen ? styles.round : ""}`}
-            disabled={disabled}
-            onClick={onClick}
-        >
-            <span>{text}</span>
-        </button>
-    );
+export default function Btn({
+  onClick,
+  text,
+  isKeyboardOpen,
+  disabled,
+}: BtnProps) {
+  return (
+    <button
+      className={`${styles.btn} ${!isKeyboardOpen ? styles.round : ''}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <span>{text}</span>
+    </button>
+  );
 }
