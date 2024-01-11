9f9b195fd30cdf2ee1ad2018905b7b447c31ad02
parkcoool <yickle1345@naver.com>
2024-01-11 18:09:35 +0900

    Feat: CheckList 및 CheckListContainer 추가

---
+++ src/components/CheckListContainer/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './Btn.module.css';
+
+import onIcon from '../../assets/img/checkList/on.svg';
+import offIcon from '../../assets/img/checkList/off.svg';
+import dragIcon from '../../assets/img/checkList/drag.svg';
+
+interface CheckListContainerProps {
+  name: string;
+  enabled: boolean;
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+export default function CheckListContainer({
+  name,
+  enabled = true,
+  children,
+  onClick,
+}: CheckListContainerProps) {
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.header} ${!enabled ? styles.disabled : ''}`}>
+        <div className={styles.headerLeft}>
+          <button className="imgBtn" onClick={onClick}>
+            <img src={enabled ? onIcon : offIcon}></img>
+          </button>
+          <span>{name}</span>
+        </div>
+        <div>
+          <img src={dragIcon}></img>
+        </div>
+      </div>
+      <div className={styles.checkListContainer}>{children}</div>
+    </div>
+  );
+}
