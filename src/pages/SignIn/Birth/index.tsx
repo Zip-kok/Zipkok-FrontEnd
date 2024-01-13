b3b8431a601c23dfa6f43f56202624217e3c5d8d
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-01-13 19:14:37 +0900

    Feat: 생년월일 유효성 검사 구현

--- src/pages/SignIn/Birth/index.tsx
+++ src/pages/SignIn/Birth/index.tsx
@@ -12,19 +12,42 @@ interface BirthProps {
 const Birth = ({ onConfirm }: BirthProps) => {
   const [birth, setBirth] = useState<string>('');
   const [isBirthValid, setIsBirthValid] = useState<boolean>(false);
+  const [showWarning, setShowWarning] = useState<boolean>(false);
 
   const handleChange = (e: FormEvent<HTMLInputElement>) => {
-    const target = e.currentTarget;
-    const value = target.value;
-    const isValid = value.length === 6 && /^\d+$/.test(value);
-
+    const value = e.currentTarget.value;
     setBirth(value);
-    setIsBirthValid(isValid);
+
+    if (value.length === 6) {
+      const isValid = isValidDate(value);
+      setIsBirthValid(isValid);
+      setShowWarning(!isValid); // 6자리 입력 후에만 경고 메시지 상태 업데이트
+    } else {
+      setIsBirthValid(false);
+      setShowWarning(false); // 6자리가 아닐 때는 경고 메시지 숨김
+    }
   };
 
   const handleSubmit = () => {
     if (isBirthValid) {
       onConfirm(birth);
+    }
+  };
+
+  // 입력한 날짜가 유효한지 판단하는 함수
+  const isValidDate = (birthdate: string): boolean => {
+    try {
+      const year_prefix =
+        parseInt(birthdate.substring(0, 2)) > 24 ? '19' : '20';
+      const formatted_date = `${year_prefix}${birthdate.substring(
+        0,
+        2,
+      )}-${birthdate.substring(2, 4)}-${birthdate.substring(4, 6)}`;
+
+      new Date(formatted_date);
+      return !isNaN(Date.parse(formatted_date));
+    } catch {
+      return false;
     }
   };
 
@@ -46,6 +69,9 @@ const Birth = ({ onConfirm }: BirthProps) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <div className={styles.warning}>
+        {showWarning && '유효하지 않은 생년월일입니다.'}
+      </div>
 
       <div className={styles.blank}></div>
 
