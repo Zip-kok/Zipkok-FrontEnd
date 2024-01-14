f6cb6547ca7e0d39dfaa6729ed79205d88a625ff
parkcoool <yickle1345@naver.com>
2024-01-14 18:38:27 +0900

    Feat: RangeSlider 만원 이하 단위 절삭

--- src/pages/Onboarding/Price/priceSlider/getPriceLabelString.ts
+++ src/pages/Onboarding/Price/priceSlider/getPriceLabelString.ts
@@ -1,16 +1,16 @@
 export default function getPriceLabelString(value: number) {
-  // 1십만 미만
-  if (value < 100_000) {
-    return value.toLocaleString();
+  // 0
+  if (value === 0) {
+    return '0';
   }
   // 1천만 미만
   else if (value < 10_000_000) {
-    return `${Math.floor(value / 10_000)}만`;
+    return `${Math.ceil(value / 10_000)}만`;
   }
   // 1억 미만
   else if (value < 100_000_000) {
-    return `${Math.floor(value / 1_000_000) / 10}천만`;
+    return `${Math.ceil(value / 1_000_000) / 10}천만`;
   }
   // 1억 이상
-  else return `${Math.floor(value / 10_000_000) / 10}억`;
+  else return `${Math.ceil(value / 10_000_000) / 10}억`;
 }
