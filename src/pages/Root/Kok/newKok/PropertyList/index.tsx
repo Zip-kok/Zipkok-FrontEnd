42fc29a979998b323b6b688bbe6a543f96a84348
parkcoool <yickle1345@naver.com>
2024-01-18 18:29:34 +0900

    Feat: 콕리스트 작성_매물리스트 페이지 추가

--- src/pages/Root/Kok/newKok/PropertyList/index.tsx
+++ src/pages/Root/Kok/newKok/PropertyList/index.tsx
@@ -3,7 +3,17 @@ import { useNavigate } from 'react-router-dom';
 
 import useNaviStore from '../../../../../contexts/naviStore';
 
+import TextInput from '../../../../../components/TextInput';
+import Header from '../../../../../components/Header';
+import PropertyItem from '../../../../../components/PropertyItem';
+import IconTextBtn from '../../../../../components/IconTextBtn';
+
+import mapIcon from '../../../../../assets/img/map.svg';
+import searchIcon from '../../../../../assets/img/search.svg';
+
 import styles from './PropertyList.module.css';
+
+import properties from '../../../../../models/properties';
 
 export default function PropertyList() {
   const navigate = useNavigate();
@@ -15,5 +25,43 @@ export default function PropertyList() {
     setShowNaviBar(true);
   }, []);
 
-  return <div className={styles.root}></div>;
+  return (
+    <div className={styles.root}>
+      <div className="top">
+        <Header title="콕리스트" backBtnEnabled onBack={() => navigate(-1)} />
+      </div>
+
+      <div className={styles.searchBox}>
+        <TextInput
+          placeholder="도로명, 지번 검색"
+          icon={searchIcon}
+          style="underline"
+        />
+        <IconTextBtn
+          image={mapIcon}
+          text="지도에서 위치 보기"
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className={styles.propertyContainer}>
+        {properties.map((property) => (
+          <PropertyItem
+            key={property.id}
+            id={property.id}
+            like={property.like}
+            type={property.type}
+            priceType={property.priceType}
+            price={property.price}
+            maintenanceFee={property.maintenanceFee}
+            address={property.address}
+            propertyName={property.propertyName}
+            imageUrl={property.imageUrl}
+            kokList={property.kokList}
+          />
+        ))}
+        di
+      </div>
+    </div>
+  );
 }
