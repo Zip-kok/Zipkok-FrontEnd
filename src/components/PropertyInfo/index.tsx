dc88b4deba691956026ec3d7aa284339399cdd73
parkcoool <yickle1345@naver.com>
2024-01-19 21:03:44 +0900

    Feat: PropertyInfo 컴포넌트 추가 및 적용

---
+++ src/components/PropertyInfo/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+
+import styles from './PropertyInfo.module.css';
+
+import IconBtn from '../IconBtn';
+
+import defaultThumbnail from '../../assets/img/defaultThumbnail.png';
+import mapImg from '../../assets/img/kokList/map.png';
+import linkIcon from '../../assets/img/link.svg';
+import areaIcon from '../../assets/img/structure.svg';
+import houseTypeIcon from '../../assets/img/size.svg';
+import floorIcon from '../../assets/img/floor.svg';
+import maintanenceFeeIcon from '../../assets/img/money.svg';
+
+import { PriceType, HouseType } from '../../pages/Onboarding';
+import BottomBtn from '../BottomBtn';
+
+interface PropertyInfoProps {
+  picture?: string;
+  address: string;
+  priceType: PriceType;
+  deposit?: number;
+  monthlyPrice?: number;
+  price?: number;
+  memo: string;
+  area: number;
+  houseType: HouseType;
+  floor: number;
+  maintanenceFee?: number;
+  detailAddress?: string;
+}
+
+export default function PropertyInfo({
+  picture = defaultThumbnail,
+  address,
+  priceType,
+  deposit,
+  monthlyPrice,
+  price,
+  memo,
+  area,
+  houseType,
+  floor,
+  maintanenceFee,
+  detailAddress,
+}: PropertyInfoProps) {
+  return (
+    <div className={styles.container}>
+      {/* 썸네일 */}
+      <div className={styles.thumbnail}>
+        <img src={picture} />
+      </div>
+
+      {/* 주소 */}
+      <div className={styles.address}>{address}</div>
+
+      {/* 가격 */}
+      <div className={styles.priceContainer}>
+        <span className={styles.priceType}>{priceType}</span>
+        <span className={styles.price}>
+          {
+            {
+              월세: `${deposit?.toLocaleString()} / ${monthlyPrice?.toLocaleString()}`,
+              전세: `${deposit?.toLocaleString()}`,
+              매매: `${price?.toLocaleString()}`,
+            }[priceType]
+          }
+        </span>
+      </div>
+
+      {/* 메모 */}
+      <div className={styles.memo}>{memo}</div>
+
+      {/* 기본 정보 */}
+      <div>
+        <div className={styles.header}>
+          <span className={styles.title}>기본정보</span>
+          <IconBtn
+            image={linkIcon}
+            text="상세보기"
+            onClick={() => {}}
+            gap="4px"
+            height="36px"
+            fontSize="12px"
+            fontWeight="400"
+            color="var(--gray-color-gray050_text, #4B5259)"
+          />
+        </div>
+
+        <div className={styles.details}>
+          <div className={styles.detail}>
+            <img src={areaIcon} />
+            <span>{area}평</span>
+          </div>
+          <div className={styles.detail}>
+            <img src={houseTypeIcon} />
+            <span>{houseType}</span>
+          </div>
+          <div className={styles.detail}>
+            <img src={floorIcon} />
+            <span>{floor}층</span>
+          </div>
+          <div className={styles.detail}>
+            <img src={maintanenceFeeIcon} />
+            <span>관리비 {maintanenceFee?.toLocaleString()}만원</span>
+          </div>
+        </div>
+      </div>
+
+      {/* 위치 */}
+      <div>
+        <div className={styles.header}>
+          <span className={styles.title}>위치</span>
+        </div>
+        <div className={styles.map}>
+          <img src={mapImg} />
+        </div>
+      </div>
+
+      <BottomBtn text="콕리스트 작성하기" onClick={() => {}} />
+    </div>
+  );
+}
--- src/pages/Root/Kok/newKok/CustomProperty/index.tsx
+++ src/pages/Root/Kok/newKok/CustomProperty/index.tsx
@@ -3,28 +3,28 @@ import { useNavigate } from 'react-router-dom';
 
 import styles from './CustomProperty.module.css';
 
-import BottomBtn from '../../../../../components/BottomBtn';
-import Header from '../../../../../components/Header';
-import TextInput from '../../../../../components/TextInput';
+import BeforeConfirm from './BeforeConfirm';
 
 import useRadioBtn from '../../../../../hooks/useRadioBtn';
 import useAddressStore from '../../../../../contexts/addressStore';
 import useNaviStore from '../../../../../contexts/naviStore';
 
-import searchIcon from '../../../../../assets/img/search.svg';
-
 import { HouseType, PriceType } from '../../../../Onboarding';
+import Header from '../../../../../components/Header';
+import PropertyInfo from '../../../../../components/PropertyInfo';
 
 export default function CustomProperty() {
   const address = useAddressStore((store) => store.address);
 
   const navigate = useNavigate();
 
+  const [isConfirming, setIsConfirming] = useState(false); // 등록 버튼 클릭 여부
+
   const [memo, setMemo] = useState(''); // 매물 메모
   const [deposit, setDeposit] = useState(0); // 보증금
   const [monthlyPrice, setMonthlyPrice] = useState(0); // 월세
   const [price, setPrice] = useState(0); // 매매가
-  const [maintenanceFee, setMaintenanceFee] = useState(0); // 관리비
+  const [maintanenceFee, setMaintanenceFee] = useState(0); // 관리비
   const [detailAddress, setDetailAddress] = useState('201호'); // 상세 주소
   const [area, setArea] = useState(0); // 넓이
   const [floor, setFloor] = useState(0); // 층고
@@ -75,169 +75,40 @@ export default function CustomProperty() {
         />
       </div>
 
-      <div className={styles.header}>
-        <h1>
-          콕리스트를 작성할
-          <br />
-          매물을 직접 등록해보세요.
-        </h1>
-      </div>
+      {/* 등록 전 */}
+      {!isConfirming && (
+        <BeforeConfirm
+          address={address}
+          detailAddress={detailAddress}
+          setIsConfirming={setIsConfirming}
+          setMemo={setMemo}
+          setDeposit={setDeposit}
+          setMonthlyPrice={setMonthlyPrice}
+          setPrice={setPrice}
+          setMaintanenceFee={setMaintanenceFee}
+          setDetailAddress={setDetailAddress}
+          setArea={setArea}
+          setFloor={setFloor}
+        />
+      )}
 
-      <div className={styles.body}>
-        {/* 매물 메모 */}
-        <div className={styles.inputContainer}>
-          <p className={styles.inputTitle}>매물 메모</p>
-          <TextInput
-            style="roundedBox"
-            placeholder="매물에 대한 메모를 작성해주세요. (최대 30자)"
-            defaultValue="정릉시장 파바 앞"
-            onChange={(e) => setMemo(e.currentTarget.value)}
-          />
-        </div>
-
-        {/* 필터 설정 */}
-        <div className={styles.inputContainer}>
-          <p className={styles.inputTitle}>필터 설정</p>
-
-          <div className={styles.filterContainers}>
-            {/* 가격 타입 */}
-            <PriceTypeRadioBtnContainer className={styles.filterContainer} />
-
-            {/* 집 타입 */}
-            <HouseTypeRadioBtnContainer className={styles.filterContainer} />
-          </div>
-        </div>
-
-        {/* 가격 */}
-        <div className={styles.inputContainer}>
-          {/* 월세 */}
-          {priceType === '월세' && (
-            <>
-              <p className={styles.inputTitle}>보증금 / 월세</p>
-              <div className={styles.monthlyPriceContainer}>
-                <TextInput
-                  style="roundedBox"
-                  placeholder="1000"
-                  defaultValue="1000"
-                  numberOnly
-                  onChange={(e) => setDeposit(e.currentTarget.valueAsNumber)}
-                />
-                <p>/</p>
-                <TextInput
-                  style="roundedBox"
-                  placeholder="50"
-                  defaultValue="50"
-                  numberOnly
-                  onChange={(e) =>
-                    setMonthlyPrice(e.currentTarget.valueAsNumber)
-                  }
-                />
-              </div>
-            </>
-          )}
-
-          {/* 전세 */}
-          {priceType === '전세' && (
-            <>
-              <p className={styles.inputTitle}>보증금</p>
-              <div className={styles.priceInputContainer}>
-                <TextInput
-                  style="roundedBox"
-                  placeholder="1000"
-                  defaultValue="1000"
-                  numberOnly
-                  onChange={(e) => setDeposit(e.currentTarget.valueAsNumber)}
-                />
-                <p>만원</p>
-              </div>
-            </>
-          )}
-
-          {/* 매매 */}
-          {priceType === '매매' && (
-            <>
-              <p className={styles.inputTitle}>매매가</p>
-              <div className={styles.priceInputContainer}>
-                <TextInput
-                  style="roundedBox"
-                  placeholder="1000"
-                  defaultValue="1000"
-                  numberOnly
-                  onChange={(e) => setPrice(e.currentTarget.valueAsNumber)}
-                />
-                <p>만원</p>
-              </div>
-            </>
-          )}
-        </div>
-
-        {/* 관리비 */}
-        <div className={styles.inputContainer}>
-          <p className={styles.inputTitle}>관리비</p>
-          <div className={styles.priceInputContainer}>
-            <TextInput
-              style="roundedBox"
-              placeholder="3.5"
-              defaultValue="3.5"
-              numberOnly
-              onChange={(e) => setMaintenanceFee(e.currentTarget.valueAsNumber)}
-            />
-            <p>만원</p>
-          </div>
-        </div>
-
-        {/* 주소 */}
-        <div className={styles.inputContainer}>
-          <p className={styles.inputTitle}>주소</p>
-
-          <TextInput
-            placeholder="주소 검색"
-            defaultValue={address}
-            icon={searchIcon}
-            style="roundedBox"
-            onClick={() => navigate('./locationEdit')}
-            readOnly
-          />
-          <TextInput
-            placeholder="상세 주소 입력"
-            value={detailAddress}
-            style="roundedBox"
-            onChange={(e) => setDetailAddress(e.currentTarget.value)}
-          />
-        </div>
-
-        {/* 넓이 */}
-        <div className={styles.inputContainer}>
-          <p className={styles.inputTitle}>[선택] 넓이</p>
-          <div className={styles.priceInputContainer}>
-            <TextInput
-              style="roundedBox"
-              placeholder="5"
-              defaultValue="8"
-              numberOnly
-              onChange={(e) => setArea(e.currentTarget.valueAsNumber)}
-            />
-            <p>평</p>
-          </div>
-        </div>
-
-        {/* 층고 */}
-        <div className={styles.inputContainer}>
-          <p className={styles.inputTitle}>[선택] 층고</p>
-          <div className={styles.priceInputContainer}>
-            <TextInput
-              style="roundedBox"
-              placeholder="1"
-              defaultValue="2"
-              numberOnly
-              onChange={(e) => setFloor(e.currentTarget.valueAsNumber)}
-            />
-            <p>층</p>
-          </div>
-        </div>
-      </div>
-
-      <BottomBtn text="등록하기" disabled={!canConfirm()} onClick={() => {}} />
+      {/* 등록 후 */}
+      {isConfirming && houseType && priceType && (
+        <PropertyInfo
+          picture={undefined}
+          address={address}
+          memo={memo}
+          deposit={deposit}
+          monthlyPrice={monthlyPrice}
+          price={price}
+          maintanenceFee={maintanenceFee}
+          detailAddress={detailAddress}
+          area={area}
+          floor={floor}
+          houseType={houseType}
+          priceType={priceType}
+        />
+      )}
     </div>
   );
 }
