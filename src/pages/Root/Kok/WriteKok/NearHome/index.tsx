e39eac32082b7994638d9a0fb566c8e8edacb6f5
parkcoool <yickle1345@naver.com>
2024-01-20 18:25:26 +0900

    Feat: 콕리스트 작성_체크 페이지 생성

---
+++ src/pages/Root/Kok/WriteKok/NearHome/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './NearHome.module.css';
+
+export default function NearHome() {
+  return <div className={styles.root}></div>;
+}
--- src/pages/Root/Kok/WriteKok/index.tsx
+++ src/pages/Root/Kok/WriteKok/index.tsx
@@ -1,11 +1,34 @@
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
+import NearHome from './NearHome';
+import InsideHome from './InsideHome';
+import Contract from './Contract';
+
+import useMenu from '../../../../hooks/useMenu';
 import useNaviStore from '../../../../contexts/naviStore';
 
 import styles from './WriteKok.module.css';
 
 export default function KokItem() {
+  // 상단 메뉴 설정
+  const [TopMenu, Content, menuIndex] = useMenu([
+    {
+      name: '집 주변',
+      element: <NearHome />,
+    },
+
+    {
+      name: '집 내부',
+      element: <InsideHome />,
+    },
+
+    {
+      name: '중개 / 계약',
+      element: <Contract />,
+    },
+  ]);
+
   const navigate = useNavigate();
 
   // 하단 내비게이션 바 설정
@@ -19,5 +42,10 @@ export default function KokItem() {
   // kokId가 있으면 해당 콕리스트 수정
   const { kokId } = useParams<{ kokId: string }>();
 
-  return <div className={styles.root}></div>;
+  return (
+    <div className={styles.root}>
+      <TopMenu />
+      <Content />
+    </div>
+  );
 }
