aa7773b48b5f6ed3633be6e1e6feb8d158eb7cb2
parkcoool <yickle1345@naver.com>
2024-01-21 14:38:14 +0900

    Feat: https 적용 및 Auth 페이지 로직 추가

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Auth() {
-  return <div></div>;
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const code = new URLSearchParams(location.search).get('code');
+    fetch(`https://zipkok.store/oauth/kakao/callback?code=${code}`)
+      .then((res) => res.json())
+      .then((res) => {
+        // 회원인 경우
+        if (res.code === 5000) {
+          const authTokens = res.result.authTokens;
+          // TODO: 토큰 저장
+          navigate('/');
+        }
+        // 회원이 아닌 경우
+        else if (res.code === 5001) {
+          navigate('/signup');
+        }
+      })
+      .catch((err) => {
+        alert(err);
+        navigate('/login');
+      });
+  }, []);
+
+  return <div>카카오 로그인 중...</div>;
 }
