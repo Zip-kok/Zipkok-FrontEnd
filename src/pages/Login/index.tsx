b042d5741262e732d147f3b135ec8cdebe76a9c1
parkcoool <yickle1345@naver.com>
2024-01-21 13:56:39 +0900

    Feat: 로그인 버튼 연결

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import styles from "./Login.module.css";
+import styles from './Login.module.css';
 
-import kakaoIcon from "../../assets/img/login/kakaoIcon.svg";
-import zipkokIcon from "../../assets/img/login/zipkokIcon_white.svg";
-import Splash from "../../components/Splash";
+import kakaoIcon from '../../assets/img/login/kakaoIcon.svg';
+import zipkokIcon from '../../assets/img/login/zipkokIcon_white.svg';
+import Splash from '../../components/Splash';
 
 export default function Login() {
   const [showSplash, setShowSplash] = useState(true);
@@ -14,6 +14,11 @@ export default function Login() {
     const timer = setTimeout(() => setShowSplash(false), 3000);
     return () => clearTimeout(timer);
   }, []);
+
+  function handleLogin() {
+    window.location.href =
+      'https://kauth.kakao.com/oauth/authorize?client_id=f251abb4b1d323c283685a7be4211aa4&redirect_uri=https://localhost:3000/auth&response_type=code';
+  }
 
   return (
     <div className={styles.root}>
@@ -26,12 +31,10 @@ export default function Login() {
             src={zipkokIcon}
             alt="zipkokIcon"
           />
-          <Link to="/signin" style={{ textDecoration: "none" }}>
-            <button className={styles.loginBtn}>
-              <img src={kakaoIcon} alt="kakaoIcon" />
-              <div>카카오 로그인</div>
-            </button>
-          </Link>
+          <button className={styles.loginBtn} onClick={handleLogin}>
+            <img src={kakaoIcon} alt="kakaoIcon" />
+            <div>카카오 로그인</div>
+          </button>
           <Link to="/">
             <button className={styles.notLoginBtn}>로그인 없이 둘러보기</button>
           </Link>
