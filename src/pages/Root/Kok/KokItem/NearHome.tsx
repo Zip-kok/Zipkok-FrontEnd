515101c392b2aa215d8270fcdf9387ee8fb58beb
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-01-21 16:00:49 +0900

    Feat: Options 컴포넌트, nearHome 구현

--- src/pages/Root/Kok/KokItem/NearHome.tsx
+++ src/pages/Root/Kok/KokItem/NearHome.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import styles from './KokItem.module.css'
-
+import data from '../../../../models/kokItemOuter.json' 
+import OptionsComponent from '../../../../components/Options'
 const NearHome = () => {
 
+  const {highlights,options} = data.result;
+
   return (
-    <div>
-      <div className={styles.body}>
-        
+    <div className={styles.body}>
+      <div className={styles.TagCtn}>
+        {highlights.map((tag, index) => (
+          <p className={styles.tagBtn} key={index}>
+            {tag}
+          </p>
+        ))}
       </div>
+
+      <div className={styles.optionsCtn}>
+        <OptionsComponent optionData={options} />
+      </div>
+      
+      <div className={styles.blank}/>
+      <div className={styles.blank}/>
+
     </div>
+
+
   )
 }
 
