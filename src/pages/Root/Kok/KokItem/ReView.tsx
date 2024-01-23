f0ef93baa1ed35183f54bbf5a2a5fe329b3ab62b
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-01-23 14:28:14 +0900

    Feat: KokItem 별점 추가

--- src/pages/Root/Kok/KokItem/ReView.tsx
+++ src/pages/Root/Kok/KokItem/ReView.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from './KokItem.module.css'
-import data from '../../../../models/kokItemReview.json'
+import React from 'react';
+import styles from './KokItem.module.css';
+import data from '../../../../models/kokItemReview.json';
+import StarRating from '../../../../components/StarRating';
 
 const ReView = () => {
-
-  const {impressions, facilityStarCount,infraStarCount,structureStarCount
-  ,vibeStarCount,reviewText} = data.result;
+  const {
+    impressions,
+    facilityStarCount,
+    infraStarCount,
+    structureStarCount,
+    vibeStarCount,
+    reviewText,
+  } = data.result;
 
   return (
     <div className={styles.body}>
@@ -18,20 +24,22 @@ const ReView = () => {
       </div>
       <div className={styles.starCtn}>
         <h3>매물은 어떠셨나요?</h3>
+        <StarRating label="시설" onRating={(rating) => console.log(rating)} />
+        <StarRating label="인프라" onRating={(rating) => console.log(rating)} />
+        <StarRating label="구조" onRating={(rating) => console.log(rating)} />
+        <StarRating label="분위기" onRating={(rating) => console.log(rating)} />
       </div>
-      
-      <div>
 
-      </div>
-      
+      <div></div>
+
       <div className={styles.reviewTextCtn}>
         <textarea defaultValue={reviewText} />
       </div>
 
-      <div className={styles.blank}/>
-      <div className={styles.blank}/>
+      <div className={styles.blank} />
+      <div className={styles.blank} />
     </div>
-  )
-}
+  );
+};
 
 export default ReView;
