a68d6222ff0991267671148bf0cbc66f784b1bd7
parkcoool <yickle1345@naver.com>
2024-01-25 19:48:58 +0900

    Feat: 온보딩 페이지 오류 모달 추가

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -141,35 +141,85 @@ export default function Onboarding() {
         purchaseMax,
       )
         .then((res) => {
-          switch (res.code) {
-            // 회원정보 등록/수정 성공
-            case StatusCode.MEMBER_INFO_UPDATE_SUCCESS:
-              setStep('complete');
-              break;
-            // 주소가 없거나 최대 입력 범위 초과
-            case StatusCode.ADDRESS_OVER_LENGTH:
-              setStep('location');
-              break;
-            // 위도, 경도가 없거나 범위 초과
-            case StatusCode.INVALID_LAT_LNG:
-              setStep('location');
-              break;
-            // 최소 가격이 없거나 0 이하
-            case StatusCode.INVALID_MIN_PRICE:
-              setStep('price');
-              break;
-            // 최대 가격이 없거나 0 이하
-            case StatusCode.INVALID_MAX_PRICE:
-              setStep('price');
-              break;
-            // 관심매물유형 오류
-            case StatusCode.INVALID_INTEREST_TYPE:
-              setStep('type');
-              break;
+          // 회원정보 등록/수정 성공
+          if (res.code === StatusCode.MEMBER_INFO_UPDATE_SUCCESS) {
+            setStep('complete');
+          } else {
+            let isDefinedError = false;
+            let errorStep: Step;
+            switch (res.code) {
+              // 주소가 없거나 최대 입력 범위 초과
+              case StatusCode.ADDRESS_OVER_LENGTH:
+                isDefinedError = true;
+                errorStep = 'location';
+                break;
+              // 위도, 경도가 없거나 범위 초과
+              case StatusCode.INVALID_LAT_LNG:
+                isDefinedError = true;
+                errorStep = 'location';
+                break;
+              // 최소 가격이 없거나 0 이하
+              case StatusCode.INVALID_MIN_PRICE:
+                isDefinedError = true;
+                errorStep = 'price';
+                break;
+              // 최대 가격이 없거나 0 이하
+              case StatusCode.INVALID_MAX_PRICE:
+                isDefinedError = true;
+                errorStep = 'price';
+                break;
+              // 관심매물유형 오류
+              case StatusCode.INVALID_INTEREST_TYPE:
+                isDefinedError = true;
+                errorStep = 'type';
+                break;
+            }
+
+            if (isDefinedError) {
+              modalStore.setModal({
+                enabled: true,
+                title: '오류가 발생했어요.',
+                description: res.message,
+                secondaryButton: '나중에 설정하기',
+                onSecondaryButtonClick: () => {
+                  modalStore.setModal({ enabled: false });
+                  navigate('/');
+                },
+                primaryButton: '다시 설정하기',
+                onPrimaryButtonClick: () => {
+                  modalStore.setModal({ enabled: false });
+                  setStep(errorStep);
+                },
+              });
+            } else {
+              modalStore.setModal({
+                enabled: true,
+                title: '오류가 발생했어요.',
+                description: res.message,
+                secondaryButton: undefined,
+                onSecondaryButtonClick: undefined,
+                primaryButton: '나중에 설정하기',
+                onPrimaryButtonClick: () => {
+                  modalStore.setModal({ enabled: false });
+                  navigate('/');
+                },
+              });
+            }
           }
         })
         .catch((err) => {
-          alert(err.message);
+          modalStore.setModal({
+            enabled: true,
+            title: '서버에 연결할 수 없어요.',
+            description: err.message,
+            secondaryButton: undefined,
+            onSecondaryButtonClick: undefined,
+            primaryButton: '나중에 설정하기',
+            onPrimaryButtonClick: () => {
+              modalStore.setModal({ enabled: false });
+              navigate('/');
+            },
+          });
         });
     },
     [],
