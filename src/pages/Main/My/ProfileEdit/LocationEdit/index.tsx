3b6516ee2b4dccd6b25e3de6550ec665d01ff827
parkcoool <yickle1345@naver.com>
2024-01-25 17:21:55 +0900

    Fix: 프로필 수정 페이지 주소 등록

--- src/pages/Main/My/ProfileEdit/LocationEdit/index.tsx
+++ src/pages/Main/My/ProfileEdit/LocationEdit/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { AddressSearchPage } from 'components';
 import useAddressStore from 'contexts/addressStore';
 
 export default function LocationEdit() {
+  const navigate = useNavigate();
   const setAddress = useAddressStore((state) => state.setAddress);
 
   return (
     <AddressSearchPage
-      confirmLocation={(address) => setAddress(address)}
+      confirmLocation={(address) => {
+        setAddress(address);
+        navigate(-1);
+      }}
       skippable={true}
       defaultAddress=""
     />
