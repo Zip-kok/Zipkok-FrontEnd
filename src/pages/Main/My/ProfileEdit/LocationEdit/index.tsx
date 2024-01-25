c1a257932e5c7863315e32772d07b1b99566c8ea
parkcoool <yickle1345@naver.com>
2024-01-25 14:35:14 +0900

    Fix: 주소 검색 돌아가기

--- src/pages/Main/My/ProfileEdit/LocationEdit/index.tsx
+++ src/pages/Main/My/ProfileEdit/LocationEdit/index.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { AddressSearchPage } from 'components';
 import useAddressStore from 'contexts/addressStore';
+import useUIStore from 'contexts/uiStore';
 
 export default function LocationEdit() {
+  const ui = useUIStore();
+  useEffect(() => {
+    ui.setUI((state) => ({
+      ...state,
+      headerTitle: '프로필 수정하기',
+      headerIcon: undefined,
+      headerBackButtonEnabled: true,
+      naviEnabled: false,
+    }));
+  }, []);
+
+  const navigate = useNavigate();
   const setAddress = useAddressStore((state) => state.setAddress);
 
   return (
     <AddressSearchPage
-      confirmLocation={(address) => setAddress(address)}
-      skippable={true}
+      confirmLocation={(address) => {
+        setAddress(address);
+        navigate(-1);
+      }}
+      skippable={false}
       defaultAddress=""
     />
   );
