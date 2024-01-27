dac22623b3b806e38bff3e5ce481b0fb36975c0c
parkcoool <yickle1345@naver.com>
2024-01-27 21:19:22 +0900

    Feat: 홈_위치검색 검색 기능 추가

--- src/pages/Main/Home/Search/index.tsx
+++ src/pages/Main/Home/Search/index.tsx
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import backIcon from 'assets/img/line(2)/left_arrow.svg';
-import { TextInput } from 'components';
 import useUIStore from 'contexts/uiStore';
+import useAddressSearch from 'hooks/useAddressSearch';
 
 import styles from './Search.module.css';
+
+import type { Address } from 'types/Address';
 
 export default function Home() {
   const ui = useUIStore();
@@ -20,6 +22,13 @@ export default function Home() {
 
   const navigate = useNavigate();
 
+  function handleAddressClick(address: Address) {
+    navigate(-1);
+  }
+
+  const [, , AddressSeachInput, AddressSearchResult, handleSubmit] =
+    useAddressSearch(handleAddressClick);
+
   return (
     <div className={styles.root}>
       {/* 검색 상자 */}
@@ -27,16 +36,19 @@ export default function Home() {
         <button className="imgBtn">
           <img src={backIcon}></img>
         </button>
-        <TextInput
+        <AddressSeachInput
           style="none"
           placeholder="어느 지역의 매물을 찾고 계신가요?"
-        ></TextInput>
+          onSubmit={handleSubmit}
+        />
       </div>
 
       {/* 최근 검색 */}
       <div className={styles.body}>
         <div className={styles.title}>최근 검색</div>
-        <div className={styles.resultContainer}></div>
+        <div className={styles.addressContainer}>
+          <AddressSearchResult />
+        </div>
       </div>
     </div>
   );
