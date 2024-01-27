b916caa28b53223ef9173c0d9b7e41a1dde237f6
parkcoool <yickle1345@naver.com>
2024-01-27 19:38:44 +0900

    Feat: 홈_위치검색 페이지 레이아웃 구현

--- src/pages/Main/Home/Search/index.tsx
+++ src/pages/Main/Home/Search/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import backIcon from 'assets/img/line(2)/left_arrow.svg';
+import { TextInput } from 'components';
 import useUIStore from 'contexts/uiStore';
 
 import styles from './Search.module.css';
@@ -18,5 +20,24 @@ export default function Home() {
 
   const navigate = useNavigate();
 
-  return <div className={styles.root}>위치 검색</div>;
+  return (
+    <div className={styles.root}>
+      {/* 검색 상자 */}
+      <div className={styles.top}>
+        <button className="imgBtn">
+          <img src={backIcon}></img>
+        </button>
+        <TextInput
+          style="roundedBox"
+          placeholder="어느 지역의 매물을 찾고 계신가요?"
+        ></TextInput>
+      </div>
+
+      {/* 최근 검색 */}
+      <div className={styles.body}>
+        <div className={styles.title}>최근 검색</div>
+        <div className={styles.resultContainer}></div>
+      </div>
+    </div>
+  );
 }
