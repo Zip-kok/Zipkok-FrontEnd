69d3397f89207156deef517edbaae10cfe5e13a4
parkcoool <yickle1345@naver.com>
2024-01-27 22:00:42 +0900

    Feat: 홈_위치검색 최근 검색 기능 추가

--- src/pages/Main/Home/Search/index.tsx
+++ src/pages/Main/Home/Search/index.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import deleteIcon from 'assets/img/line(1)/delete.svg';
 import backIcon from 'assets/img/line(2)/left_arrow.svg';
 import useUIStore from 'contexts/uiStore';
 import useAddressSearch from 'hooks/useAddressSearch';
@@ -9,8 +10,20 @@ import styles from './Search.module.css';
 
 import type { Address } from 'types/Address';
 
+interface AddressHistory {
+  id: number;
+  address: Address;
+  date: string;
+}
+
 export default function Home() {
+  const [searched, setSearched] = useState(false);
+
   const ui = useUIStore();
+  const recentSearch = JSON.parse(
+    localStorage.getItem('recentSearch') || '[]',
+  ) as AddressHistory[];
+
   useEffect(() => {
     ui.setUI((state) => ({
       ...state,
@@ -23,10 +36,21 @@ export default function Home() {
   const navigate = useNavigate();
 
   function handleAddressClick(address: Address) {
+    localStorage.setItem(
+      'recentSearch',
+      JSON.stringify([
+        {
+          id: Math.random().toString(16),
+          address,
+          date: `${new Date().getMonth() + 1}. ${new Date().getDate()}.`,
+        },
+        ...recentSearch,
+      ]),
+    );
     navigate(-1);
   }
 
-  const [, , AddressSeachInput, AddressSearchResult, handleSubmit] =
+  const [, addressCount, AddressSeachInput, AddressSearchResult, handleSubmit] =
     useAddressSearch(handleAddressClick);
 
   return (
@@ -39,16 +63,53 @@ export default function Home() {
         <AddressSeachInput
           style="none"
           placeholder="어느 지역의 매물을 찾고 계신가요?"
-          onSubmit={handleSubmit}
+          onSubmit={() => {
+            handleSubmit();
+            setSearched(true);
+          }}
         />
       </div>
 
-      {/* 최근 검색 */}
       <div className={styles.body}>
-        <div className={styles.title}>최근 검색</div>
-        <div className={styles.addressContainer}>
-          <AddressSearchResult />
-        </div>
+        {/* 최근 검색 */}
+        {!searched && (
+          <>
+            <div className={styles.title}>최근 검색</div>
+            <div className={styles.addressContainer}>
+              {recentSearch.map((history) => (
+                <RecentAddress history={history} key={history.id} />
+              ))}
+            </div>
+          </>
+        )}
+
+        {/* 검색 결과 */}
+        {searched && (
+          <>
+            <div className={styles.title}>
+              {addressCount.toLocaleString()}건의 검색 결과
+            </div>
+            <div className={styles.addressContainer}>
+              <AddressSearchResult />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function RecentAddress({ history }: { history: AddressHistory }) {
+  return (
+    <div className={styles.recent}>
+      <button className={styles.recentButton}>
+        {history.address.address_name}
+      </button>
+      <div className={styles.recentRight}>
+        <span>{history.date}</span>
+        <button className="imgBtn">
+          <img src={deleteIcon}></img>
+        </button>
       </div>
     </div>
   );
