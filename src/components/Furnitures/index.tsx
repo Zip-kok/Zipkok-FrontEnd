11a121d27553d6fe5e057b03cbccdddcca1774eb
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-01-28 15:26:07 +0900

    Feat: Furnitures 컴포넌트

---
+++ src/components/Furnitures/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import aricon from '../../assets/img/furniture/airconditioner.svg';
+import bed from '../../assets/img/furniture/bed.svg';
+import chair from '../../assets/img/furniture/chair.svg';
+import closet from '../../assets/img/furniture/closet.svg';
+import desk from '../../assets/img/furniture/desk.svg';
+import dryer from '../../assets/img/furniture/dryer.svg';
+import fridge from '../../assets/img/furniture/fridge.svg';
+import microwave from '../../assets/img/furniture/microwave.svg';
+import shoe from '../../assets/img/furniture/shoe.svg';
+import sink from '../../assets/img/furniture/sink.svg';
+import stove from '../../assets/img/furniture/stove.svg';
+import washer from '../../assets/img/furniture/washer.svg';
+
+const Furnitures = [
+  {
+    id: 1,
+    name: '전자레인지',
+    img: microwave,
+  },
+  {
+    id: 2,
+    name: '인덕션',
+    img: stove,
+  },
+  {
+    id: 3,
+    name: '에어컨',
+    img: aricon,
+  },
+  {
+    id: 4,
+    name: '냉장고',
+    img: fridge,
+  },
+  {
+    id: 5,
+    name: '세탁기',
+    img: washer,
+  },
+  {
+    id: 6,
+    name: '건조기',
+    img: dryer,
+  },
+  {
+    id: 7,
+    name: '침대',
+    img: bed,
+  },
+  {
+    id: 8,
+    name: '책상',
+    img: desk,
+  },
+  {
+    id: 9,
+    name: '옷장',
+    img: closet,
+  },
+  {
+    id: 10,
+    name: '싱크대',
+    img: sink,
+  },
+  {
+    id: 11,
+    name: '의자',
+    img: chair,
+  },
+  {
+    id: 12,
+    name: '신발장',
+    img: shoe,
+  },
+];
+
+export default Furnitures;
