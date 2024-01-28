09be7d420f401eafc7e099e22a5c4ba55366a49f
parkcoool <yickle1345@naver.com>
2024-01-28 12:27:42 +0900

    Feat: 홈 페이지에 바텀시트 추가

--- src/pages/Main/Home/index.tsx
+++ src/pages/Main/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import BottomSheet from 'components/BottomSheet';
@@ -8,6 +8,8 @@ import { Filter } from './components';
 import styles from './Home.module.css';
 
 export default function Home() {
+  const [filterOpen, setFilterOpen] = useState(false);
+
   const ui = useUIStore();
   useEffect(() => {
     ui.setUI((state) => ({
@@ -20,5 +22,34 @@ export default function Home() {
 
   const navigate = useNavigate();
 
-  return <div className={styles.root}>홈</div>;
+  // 테스트용
+  function handleFilterClick() {
+    setFilterOpen((prev) => !prev);
+  }
+
+  function handleOverlayClick() {
+    setFilterOpen(false);
+  }
+
+  return (
+    <div className={styles.root}>
+      {/* 필터 */}
+      {filterOpen && (
+        <>
+          <div className={styles.bottomSheet}>
+            <BottomSheet>
+              <Filter />
+            </BottomSheet>
+          </div>
+          <div className={styles.overlay} onClick={handleOverlayClick}></div>
+        </>
+      )}
+
+      {/* 리스트뷰 */}
+      {}
+
+      {/* 지도 */}
+      <button onClick={handleFilterClick}>filter</button>
+    </div>
+  );
 }
