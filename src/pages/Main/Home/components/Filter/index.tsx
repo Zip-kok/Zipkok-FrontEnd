81fec8630da9d7a836a4e23badb2888a7ca0bc54
parkcoool <yickle1345@naver.com>
2024-01-28 12:21:44 +0900

    Feat: BottomSheet 및 Filter 컴포넌트 추가

---
+++ src/pages/Main/Home/components/Filter/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './Filter.module.css';
+
+export default function Filter() {
+  return <></>;
+}
--- src/pages/Main/Home/index.tsx
+++ src/pages/Main/Home/index.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import BottomSheet from 'components/BottomSheet';
 import useUIStore from 'contexts/uiStore';
 
+import { Filter } from './components';
 import styles from './Home.module.css';
 
 export default function Home() {
