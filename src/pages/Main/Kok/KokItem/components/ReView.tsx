712c35a7a8dc5a362369b3cdf6853c41b5c79ff3
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-01-29 16:24:08 +0900

    Feat: KokItem Review readonly 적용

--- src/pages/Main/Kok/KokItem/components/ReView.tsx
+++ src/pages/Main/Kok/KokItem/components/ReView.tsx
@@ -29,10 +29,27 @@ const ReView = () => {
         <StarRating
           label="시설"
           onRating={(facilityStarCount) => facilityStarCount}
+          readOnly={true}
+          starCount={facilityStarCount}
         />
-        <StarRating label="인프라" onRating={(rating) => console.log(rating)} />
-        <StarRating label="구조" onRating={(rating) => console.log(rating)} />
-        <StarRating label="분위기" onRating={(rating) => console.log(rating)} />
+        <StarRating
+          label="인프라"
+          onRating={(rating) => console.log(rating)}
+          readOnly={true}
+          starCount={infraStarCount}
+        />
+        <StarRating
+          label="구조"
+          onRating={(rating) => console.log(rating)}
+          readOnly={true}
+          starCount={structureStarCount}
+        />
+        <StarRating
+          label="분위기"
+          onRating={(rating) => console.log(rating)}
+          readOnly={true}
+          starCount={vibeStarCount}
+        />
       </div>
 
       <div></div>
