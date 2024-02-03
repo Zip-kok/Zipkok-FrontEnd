5b9f79787a8f7f499b3e417f405860e7ea346ec7
parkcoool <yickle1345@naver.com>
2024-02-03 17:58:53 +0900

    Feat: 회원가입 오류 모달 추가

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { signIn } from 'apis';
 import leftArrowIcon from 'assets/img/line(2)/left_arrow.svg';
 import useEmailStore from 'contexts/emailStore';
+import useModal from 'contexts/modalStore';
 import { StatusCode } from 'types/StatusCode';
 
 import Birth from './Birth';
@@ -18,6 +19,7 @@ export type Gender = '남자' | '여자' | '비공개';
 export default function SignIn() {
   const [step, setStep] = useState<Step>('nickname');
   const navigate = useNavigate();
+  const modal = useModal();
 
   const email = useEmailStore((store) => store.email);
   const [nickname, setNickname] = useState<string>('');
@@ -64,33 +66,65 @@ export default function SignIn() {
     (nickname: string, gender: Gender, birth: Date) => {
       signIn(nickname, 'KAKAO', email, gender, birth)
         .then((res) => {
-          switch (res.code) {
-            // 성공적으로 회원가입 성공
-            case StatusCode.REGISTRATION_SUCCESS:
-              setStep('complete');
-              break;
-            // 닉네임이 없거나 잘못된 형식
-            case StatusCode.INVALID_NICKNAME_FORMAT:
-              alert(res.message);
-              setStep('nickname');
-              break;
-            // 성별이 없거나 잘못된 형식
-            case StatusCode.INVALID_GENDER_FORMAT:
-              alert(res.message);
-              setStep('gender');
-              break;
-            // 생년월일이 없거나 잘못된 형식
-            case StatusCode.INVALID_BIRTHDAY_FORMAT:
-              alert(res.message);
-              setStep('birth');
-              break;
-            default:
-              alert(res.message);
-              break;
+          // 회원가입 성공
+          if (res.code === StatusCode.REGISTRATION_SUCCESS) {
+            setStep('complete');
+          } else {
+            let isDefinedError = false;
+            let errorStep: Step;
+            switch (res.code) {
+              // 닉네임이 없거나 잘못된 형식
+              case StatusCode.INVALID_NICKNAME_FORMAT:
+                isDefinedError = true;
+                errorStep = 'nickname';
+                break;
+              // 성별이 없거나 잘못된 형식
+              case StatusCode.INVALID_GENDER_FORMAT:
+                isDefinedError = true;
+                errorStep = 'gender';
+                break;
+              // 생년월일이 없거나 잘못된 형식
+              case StatusCode.INVALID_BIRTHDAY_FORMAT:
+                isDefinedError = true;
+                errorStep = 'birth';
+                break;
+            }
+
+            if (isDefinedError) {
+              modal
+                .open({
+                  title: '올바르지 않은 값이 있어요.',
+                  description: res.message,
+                  primaryButton: '다시 설정하기',
+                  secondaryButton: '나중에 설정하기',
+                })
+                .then((res) => {
+                  if (res === 'primary') setStep(errorStep);
+                  else navigate('/');
+                });
+            } else {
+              modal
+                .open({
+                  title: '오류가 발생했어요.',
+                  description: res.message,
+                  primaryButton: '나중에 설정하기',
+                })
+                .then((res) => {
+                  if (res === 'primary') navigate('/');
+                });
+            }
           }
         })
         .catch((err) => {
-          alert(err.message);
+          modal
+            .open({
+              title: '오류가 발생했어요.',
+              description: err.message,
+              primaryButton: '나중에 설정하기',
+            })
+            .then((res) => {
+              if (res === 'primary') navigate('/');
+            });
         });
     },
     [],
