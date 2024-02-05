cf4e570895de33272c43ac50a98d1b865ee23dab
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-02-05 19:45:43 +0900

    Feat: Filter 추가

--- src/pages/Main/Home/components/Filter/index.tsx
+++ src/pages/Main/Home/components/Filter/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import BottomBtn from 'components/BottomBtn';
+import useMyPageStore from 'contexts/useMyPageStore';
 import useRadioBtn from 'hooks/useRadioBtn';
 import Jeonse from 'pages/Onboarding/Price/priceSlider/Jeonse';
 import Monthly from 'pages/Onboarding/Price/priceSlider/Monthly';
@@ -7,6 +9,7 @@ import Purchase from 'pages/Onboarding/Price/priceSlider/Purchase';
 
 import styles from './Filter.module.css';
 import { Gender } from '../../../../SignIn';
+import BottomSheetHeader from '../../BottomSheet/HomeBottomSheet/BottomSheetHeader';
 
 import type { HouseType } from 'types/HouseType';
 import type { PriceType } from 'types/PriceType';
@@ -49,9 +52,52 @@ export default function Filter() {
   );
 
   const [priceRanges, setPriceRanges] = useState<PriceRange[]>([]);
+  const {
+    setRealEstateType,
+    setTransactionType,
+    setPriceMax,
+    setDepositMax,
+    setPriceMin,
+    setDepositMin,
+  } = useMyPageStore();
+
+  const handelSaveBtnClick = () => {
+    setRealEstateType(houseType!);
+    setTransactionType(priceType!);
+
+    // 가격 범위를 업데이트합니다.
+    // 가격 유형에 따라 최대/최소 가격과 보증금을 다르게 설정합니다.
+    if (priceType === '월세') {
+      // 월세의 경우, 첫 번째 배열 요소는 보증금 범위, 두 번째는 월세 범위입니다.
+      const [depositRange, monthlyRange] = priceRanges;
+      setDepositMin(depositRange[0]);
+      setDepositMax(depositRange[1]);
+      setPriceMin(monthlyRange[0]);
+      setPriceMax(monthlyRange[1]);
+    } else if (priceType === '전세') {
+      // 전세의 경우, 보증금 범위만 설정합니다.
+      const [depositRange] = priceRanges;
+      setDepositMin(depositRange[0]);
+      setDepositMax(depositRange[1]);
+      // 전세는 월세가 없으므로 가격을 0으로 설정할 수 있습니다.
+      setPriceMin(0);
+      setPriceMax(0);
+    } else if (priceType === '매매') {
+      // 매매의 경우, 가격 범위만 설정합니다.
+      const [purchaseRange] = priceRanges;
+      setPriceMin(purchaseRange[0]);
+      setPriceMax(purchaseRange[1]);
+      // 매매는 보증금이 없으므로 보증금을 0으로 설정할 수 있습니다.
+      setDepositMin(0);
+      setDepositMax(0);
+    }
+  };
 
   return (
     <div className={styles.root}>
+      {/* 바텀시트 헤더 */}
+      <BottomSheetHeader />
+      {/* 바텀 시트 Content */}
       {/* 필터 설정 */}
       <div className={styles.container}>
         <h1 className={styles.title}>필터 설정</h1>
@@ -65,7 +111,7 @@ export default function Filter() {
         </div>
 
         {/* 가격 설정 */}
-        <div className={styles.container}>
+        <div className={styles.priceContainer}>
           {priceType === '월세' && (
             <Monthly
               onChange1={(rangeStart, rangeEnd) => {
@@ -97,6 +143,8 @@ export default function Filter() {
           )}
         </div>
       </div>
+      <div style={{ height: '70px' }}></div>
+      <BottomBtn text="저장" onClick={handelSaveBtnClick} />
     </div>
   );
 }
--- src/pages/Main/Home/index.tsx
+++ src/pages/Main/Home/index.tsx
@@ -3,14 +3,16 @@ import { useNavigate } from 'react-router-dom';
 
 import BottomSheet from 'components/BottomSheet';
 import useUIStore from 'contexts/uiStore';
+import useMyPageStore from 'contexts/useMyPageStore';
 
 import HomeBottomSheet from './BottomSheet';
-import { Filter } from './components';
+import { Filter, SearchBox } from './components';
 import styles from './Home.module.css';
 import KakaoMap from './KakaoMap';
 
 export default function Home() {
   const [filterOpen, setFilterOpen] = useState(false);
+  const [filterSet, setFilterSet] = useState(false);
 
   const ui = useUIStore();
   useEffect(() => {
@@ -22,6 +24,21 @@ export default function Home() {
     }));
   }, []);
 
+  const { realEstateType, transactionType, priceMax, depositMax } =
+    useMyPageStore((state) => ({
+      realEstateType: state.realEstateType,
+      transactionType: state.transactionType,
+      priceMax: state.priceMax,
+      depositMax: state.depositMax,
+    }));
+  useEffect(() => {
+    if (realEstateType) {
+      setFilterSet(true);
+    } else {
+      setFilterSet(false);
+    }
+  }, [realEstateType, transactionType, priceMax, depositMax]);
+
   const navigate = useNavigate();
 
   // 테스트용
@@ -32,6 +49,13 @@ export default function Home() {
   function handleOverlayClick() {
     setFilterOpen(false);
   }
+
+  // 필터 적용 버튼 클릭 핸들러
+  const applyFilter = () => {
+    // 필터를 적용하는 로직을 여기에 추가
+    setFilterSet(true);
+    setFilterOpen(false);
+  };
 
   return (
     <div className={styles.root}>
@@ -46,12 +70,38 @@ export default function Home() {
           <div className={styles.overlay} onClick={handleOverlayClick}></div>
         </>
       )}
+      <SearchBox></SearchBox>
 
-      {/* 리스트뷰 */}
-      {}
+      {/* 필터가 설정되었을 때와 설정되지 않았을 때의 화면 */}
+      {filterSet ? (
+        // 필터가 설정되었을 때 보여줄 화면
+        <div className={styles.filterCtn}>
+          <div className={styles.filter} onClick={handleFilterClick}>
+            {realEstateType}
+          </div>
+          <div className={styles.filter} onClick={handleFilterClick}>
+            {transactionType}
+          </div>
+          <div className={styles.filter} onClick={handleFilterClick}>
+            {priceMax && `${priceMax}이하`}
+          </div>
+          <div className={styles.filter} onClick={handleFilterClick}>
+            {depositMax && `${depositMax}이하`}
+          </div>
+        </div>
+      ) : (
+        // 필터가 설정되지 않았을 때 보여줄 화면
+        <div className={styles.notFilterCtn}>
+          <p>
+            아직 필터가 설정되지 않았어요
+            <br /> 필터를 설정해보세요!
+          </p>
+          <button className={styles.filterBtn} onClick={handleFilterClick}>
+            필터 설정하기
+          </button>
+        </div>
+      )}
 
-      {/* 지도 */}
-      <button onClick={handleFilterClick}>filter</button>
       <div>
         <KakaoMap />
       </div>
