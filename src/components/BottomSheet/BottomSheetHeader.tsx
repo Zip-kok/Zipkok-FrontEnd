e656bbb1e7eb62d4072123fe99e4eba6d0846898
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-02-06 02:48:27 +0900

    Fix: Filter BottomSheet 적용중 ..
    미완

---
+++ src/components/BottomSheet/BottomSheetHeader.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+
+import handleImg from 'assets/img/common/handle.svg';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+import styles from './BottomSheet.module.css';
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 16px;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+
+  position: relative;
+  padding: 4px 0;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+const Handle = styled.div`
+  width: 40px;
+  height: 4px;
+  border-radius: 2px;
+  background-color: #4b5259;
+  margin: auto;
+`;
+
+const BottomSheetHeader = () => {
+  return (
+    <Wrapper>
+      <Handle />
+    </Wrapper>
+  );
+};
+
+export default BottomSheetHeader;
--- src/pages/Main/Home/components/Filter/FilterBottomSheetOption.ts
+++
@@ -1,7 +0,0 @@
-// MIN_Y : 바텀 시트가 최대로 올라 갔을 때의 Y좌표 값
-// MAX_Y : 바텀 시트가 최대로 내려갔을 때의 Y좌표 값
-// BOTTOM_SHEET_HEIGHT : 바텀시트 높이
-
-export const MIN_Y = 125;
-export const MAX_Y = window.innerHeight - 165;
-export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y;
