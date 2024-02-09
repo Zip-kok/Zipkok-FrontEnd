606a9644bdc39a4121b4e031f62eabd20a2573a3
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-02-09 18:56:53 +0900

    Feat: api 함수 작성

---
+++ src/apis/getKokDetail.ts
@@ -0,0 +1,52 @@
+import api from '.';
+
+import type { HouseType } from 'types/HouseType';
+import type { PriceType } from 'types/PriceType';
+import type { ZipkokResponse } from 'types/ZipkokResponse';
+
+export interface KokDetail {
+  kokId: number;
+  imageInfo: imageInfo[];
+  address: string;
+  detailAddress: string;
+  transactionType: PriceType;
+  deposit: number;
+  price: number;
+  detail: string;
+  areaSize: number;
+  pyeongsu: number;
+  realEstateType: HouseType;
+  floorNum: number;
+  administraiveFee: number;
+  langtitude: number;
+  longtitude: number;
+  isZimmed: boolean;
+}
+export interface imageInfo {
+  imageNumber: number;
+  imageUrls: string[];
+}
+/**
+ * `GET /kok/{kokId}/detail`
+ * 콕리스트에서 특정 콕을 클릭시 호출되는 API
+ * 콕의 세부정보 정보 반환
+ * {kokId} : 집 주변 정보 조회를 원하는 콕의 ID
+ * @param number kokId
+ */
+export async function getKokDetail(kokId: number) {
+  const path = '/kok/${kokId}/detail';
+  const method = 'GET';
+  const params = {};
+  const authRequired = true;
+
+  const res = await api<ZipkokResponse<KokDetail>>(
+    path,
+    method,
+    authRequired,
+    params,
+    undefined,
+    undefined,
+  );
+
+  return res as ZipkokResponse<KokDetail>;
+}
---
+++ src/apis/getKokInner.ts
@@ -0,0 +1,38 @@
+import api from '.';
+
+import type { ZipkokResponse } from 'types/ZipkokResponse';
+
+export interface KokInner {
+  furnitureOptions: string[];
+  direction: string;
+  options: options[];
+}
+export interface options {
+  option: string;
+  orderNumber: number;
+  detailOption: string[];
+}
+/**
+ * `GET /kok/{kokId}/inner`
+ * 콕리스트_작성한리스트 확인 (5가지 항목) 에서 “집 주변” 탭 클릭시 호출되는 API
+ * 콕의 집 내부 정보 반환
+ * {kokId} : 집 주변 정보 조회를 원하는 콕의 ID
+ * @param number kokId
+ */
+export async function getKokInner(kokId: number) {
+  const path = '/kok/${kokId}/inner';
+  const method = 'GET';
+  const params = {};
+  const authRequired = true;
+
+  const res = await api<ZipkokResponse<KokInner>>(
+    path,
+    method,
+    authRequired,
+    params,
+    undefined,
+    undefined,
+  );
+
+  return res as ZipkokResponse<KokInner>;
+}
