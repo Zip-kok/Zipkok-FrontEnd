a5a02fe29e99e96a3b87b75c67953c2ea042a656
parkcoool <yickle1345@naver.com>
2024-02-14 19:05:45 +0900

    Feat: PinWrite 지도 주소 연동 구현

--- src/pages/Main/My/Pin/Write/components/Map/KakaoMap.tsx
+++ src/pages/Main/My/Pin/Write/components/Map/KakaoMap.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
 
 import styles from './Map.module.css';
 
@@ -10,93 +15,104 @@ declare global {
   }
 }
 
+export interface KakaoMapRef {
+  getCurrentPosBtn: () => void;
+}
+
 interface KakaoMapProps {
   setAddress: (address: Address) => void;
 }
 
-const KakaoMap = ({ setAddress }: KakaoMapProps) => {
-  const [map, setMap] = useState<any>();
-  const [coord, setCoord] = useState<[number, number]>();
+const KakaoMap = forwardRef<KakaoMapRef, KakaoMapProps>(
+  ({ setAddress }: KakaoMapProps, ref) => {
+    const [map, setMap] = useState<any>();
+    const [coord, setCoord] = useState<[number, number]>();
 
-  const geocoder = new kakao.maps.services.Geocoder();
+    const geocoder = new kakao.maps.services.Geocoder();
 
-  // coord가 변경될 때마다 지도의 중심을 변경
-  useEffect(() => {
-    if (map === undefined) return;
-    if (coord === undefined) return;
+    // coord가 변경될 때마다 지도의 중심을 변경
+    useEffect(() => {
+      if (map === undefined) return;
+      if (coord === undefined) return;
 
-    const currentPos = new window.kakao.maps.LatLng(coord[0], coord[1]);
-    map.panTo(currentPos);
-  }, [map, coord]);
+      const currentPos = new window.kakao.maps.LatLng(coord[0], coord[1]);
+      map.panTo(currentPos);
+    }, [map, coord]);
 
-  // 전달된 lat 및 lng가 없을 시
-  useEffect(() => {
-    getCurrentPosBtn();
-  }, []);
+    // 전달된 lat 및 lng가 없을 시
+    useEffect(() => {
+      getCurrentPosBtn();
+    }, []);
 
-  // 1) 지도 생성
-  useEffect(() => {
-    window.kakao.maps.load(() => {
-      const container = document.getElementById('map');
-      const options = {
-        center: new window.kakao.maps.LatLng(37.5665, 126.978),
-        level: 3,
-      };
+    // 1) 지도 생성
+    useEffect(() => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(37.5665, 126.978),
+          level: 3,
+        };
 
-      setMap(new window.kakao.maps.Map(container, options));
-    });
-  }, []);
+        setMap(new window.kakao.maps.Map(container, options));
+      });
+    }, []);
 
-  // 2) 현재 위치 함수
-  const getCurrentPosBtn = () => {
-    navigator.geolocation.getCurrentPosition(
-      getPosSuccess,
-      () => {
-        // 현재 위치 가져오기 실패 시 서울로 이동
-        setCoord([37.5665, 126.978]);
-      },
-      {
-        enableHighAccuracy: true,
-        maximumAge: 30000,
-        timeout: 27000,
-      },
-    );
-  };
-
-  // 3) 정상적으로 현재위치 가져올 경우 실행
-  const getPosSuccess = (pos: GeolocationPosition) => {
-    const { latitude, longitude } = pos.coords;
-    setCoord([latitude, longitude]);
-  };
-
-  useEffect(() => {
-    if (map === undefined) return;
-
-    const idleHandler = () => {
-      const center = map.getCenter();
-      geocoder.coord2Address(
-        center.getLng(),
-        center.getLat(),
-        (result: any) => {
-          setAddress({
-            address_name: result[0].address.address_name,
-            x: center.getLat(),
-            y: center.getLng(),
-          });
+    // 2) 현재 위치 함수
+    const getCurrentPosBtn = () => {
+      navigator.geolocation.getCurrentPosition(
+        getPosSuccess,
+        () => {
+          // 현재 위치 가져오기 실패 시 서울로 이동
+          setCoord([37.5665, 126.978]);
+        },
+        {
+          enableHighAccuracy: true,
+          maximumAge: 30000,
+          timeout: 27000,
         },
       );
     };
 
-    // idle 이벤트에 대한 이벤트 리스너 추가
-    window.kakao.maps.event.addListener(map, 'idle', idleHandler);
+    useImperativeHandle(ref, () => ({
+      getCurrentPosBtn,
+    }));
 
-    // 컴포넌트가 언마운트되면 이벤트 리스너 제거
-    return () => {
-      window.kakao.maps.event.removeListener(map, 'idle', idleHandler);
+    // 3) 정상적으로 현재위치 가져올 경우 실행
+    const getPosSuccess = (pos: GeolocationPosition) => {
+      const { latitude, longitude } = pos.coords;
+      setCoord([latitude, longitude]);
     };
-  }, [map]);
 
-  return <div className={styles.map} id="map"></div>;
-};
+    useEffect(() => {
+      if (map === undefined) return;
+
+      const idleHandler = () => {
+        const center = map.getCenter();
+        geocoder.coord2Address(
+          center.getLng(),
+          center.getLat(),
+          (result: any) => {
+            setAddress({
+              address_name:
+                result[0]?.address?.address_name ?? '등록되지 않은 주소',
+              x: center.getLat(),
+              y: center.getLng(),
+            });
+          },
+        );
+      };
+
+      // idle 이벤트에 대한 이벤트 리스너 추가
+      window.kakao.maps.event.addListener(map, 'idle', idleHandler);
+
+      // 컴포넌트가 언마운트되면 이벤트 리스너 제거
+      return () => {
+        window.kakao.maps.event.removeListener(map, 'idle', idleHandler);
+      };
+    }, [map]);
+
+    return <div className={styles.map} id="map"></div>;
+  },
+);
 
 export default KakaoMap;
--- src/pages/Main/My/Pin/Write/components/Map/index.tsx
+++ src/pages/Main/My/Pin/Write/components/Map/index.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
-import KakaoMap from './KakaoMap';
+import locationIcon from 'assets/img/line(1)/location.svg';
+
+import KakaoMap, { KakaoMapRef } from './KakaoMap';
 import styles from './Map.module.css';
 
 import type { Address } from 'types/Address';
@@ -10,11 +12,22 @@ interface MapProps {
 }
 
 export default function Map({ confirmLocation }: MapProps) {
+  const mapRef = useRef<KakaoMapRef>(null);
   const [address, setAddress] = useState<Address>();
 
   return (
     <div className={styles.root}>
-      <KakaoMap setAddress={setAddress} />
+      <KakaoMap setAddress={setAddress} ref={mapRef} />
+      <div className={styles.bottom}>
+        <h1>{address?.address_name}</h1>
+        <button
+          className={`imgBtn ${styles.locationBtn}`}
+          onClick={mapRef.current?.getCurrentPosBtn}
+        >
+          <img src={locationIcon} />
+          <span>현재 위치로 설정</span>
+        </button>
+      </div>
     </div>
   );
 }
