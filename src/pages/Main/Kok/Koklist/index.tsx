745e0ccfaf8ff67422f7bcd8223935a10d468cdf
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-02-16 02:03:52 +0900

    feat: 콕리스트 무한스크롤

--- src/pages/Main/Kok/Koklist/index.tsx
+++ src/pages/Main/Kok/Koklist/index.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { useNavigate } from 'react-router-dom';
 
 import { getUserKokList } from 'apis';
@@ -6,8 +7,10 @@ import { UserKokList } from 'apis/getUserKokList';
 import checkIcon from 'assets/img/line(2)/check.svg';
 import { ReactComponent as PenIcon } from 'assets/img/line(2)/pen_white.svg';
 import { BottomBtn, PropertyItem } from 'components';
+import useModal from 'contexts/modalStore';
 import useUIStore from 'contexts/uiStore';
 import properties from 'models/properties';
+import { StatusCode } from 'types/StatusCode';
 
 import styles from './Kok.module.css';
 import { realEstateInfo } from '../../Home/KakaoMap/index';
@@ -15,9 +18,31 @@ import { realEstateInfo } from '../../Home/KakaoMap/index';
 export default function Koklist() {
   const ui = useUIStore();
   const [propertyList, setPropertyList] = useState<UserKokList>();
+
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  //페이지 끝에 도달했을때 추가 데이터를 가져오는 함수(무한스크롤 관련 함수)
+  const getMoreData = useCallback(() => {
+    //1페이지기준 6개씩 가져옴
+    getUserKokList(page + 1, 6).then((res) => {
+      if (res.code == 7008) {
+        setHasMore(false);
+        return;
+      }
+      setPropertyList((prevList) => ({
+        ...res.result,
+        koks: [...(prevList?.koks ?? []), ...res.result.koks],
+      }));
+      setPage(page + 1);
+    });
+  }, [page]);
   useEffect(() => {
-    getUserKokList(1, 1).then((res) => {
-      console.log(res);
+    getUserKokList(page, 6).then((res) => {
+      if (res.code == 7008) {
+        return;
+      } else if (res.code == 5015) {
+      }
       setPropertyList(res.result);
     });
     ui.setUI((state) => ({
@@ -43,23 +68,33 @@ export default function Koklist() {
   return (
     <div className={styles.root}>
       <div className={styles.propertyContainer}>
-        {propertyList &&
-          propertyList.koks.map((property: any) => (
-            <PropertyItem
-              key={property.kokId}
-              id={property.kokId}
-              like={property.isZimmed}
-              type={property.realEstateType}
-              priceType={property.transactionType}
-              price={property.price}
-              deposit={property.deposit}
-              address={property.address}
-              propertyName={property.estateAgent}
-              imageUrl={property.imageUrl}
-              kokList={true}
-              onClick={() => handlePropertyClick(property.kokId)}
-            />
-          ))}
+        <InfiniteScroll
+          dataLength={propertyList?.koks.length || 0}
+          next={getMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          {propertyList && propertyList.koks.length > 0 ? (
+            propertyList.koks.map((property: any) => (
+              <PropertyItem
+                key={property.kokId}
+                id={property.kokId}
+                like={property.isZimmed}
+                type={property.realEstateType}
+                priceType={property.transactionType}
+                price={property.price}
+                deposit={property.deposit}
+                address={property.address}
+                propertyName={property.estateAgent}
+                imageUrl={property.imageUrl}
+                kokList={true}
+                onClick={() => handlePropertyClick(property.kokId)}
+              />
+            ))
+          ) : (
+            <div className={styles.noKokList}>콕 리스트가 없습니다.</div>
+          )}
+        </InfiniteScroll>
       </div>
 
       <BottomBtn
