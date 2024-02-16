bd91beb4af545682b1afb3383659ce7b076c1033
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-02-17 00:43:58 +0900

    Fix: 마이페이지 priceType 영어로 보이는 오류 수정

--- src/pages/Main/My/MyPage/index.tsx
+++ src/pages/Main/My/MyPage/index.tsx
@@ -17,6 +17,7 @@ import useModal from 'contexts/modalStore';
 import useUIStore from 'contexts/uiStore';
 import useMyPageStore from 'contexts/useMyPageStore';
 import convertHouseTypeToString from 'utils/convertHouseTypeToString';
+import convertPriceTypeToString from 'utils/convertPriceTypeToString';
 import getPriceString from 'utils/getPriceString';
 import isLoggedIn from 'utils/isLoggedIn';
 import logout from 'utils/logout';
@@ -165,7 +166,7 @@ const Mypage = () => {
 
           {MyPageStore.transactionType && MyPageStore.realEstateType && (
             <div className={styles.tag}>
-              <p>{MyPageStore.transactionType}</p>
+              <p>{convertPriceTypeToString(MyPageStore.transactionType)}</p>
               <p>{convertHouseTypeToString(MyPageStore.realEstateType)}</p>
               {MyPageStore.transactionType && (
                 <p>
