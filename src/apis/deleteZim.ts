2e0cc2196aec233d7d3c21bd1cfbdfa347d501b1
Ji Ho June <129824629+ho0010@users.noreply.github.com>
2024-02-17 07:22:02 +0900

    Feat: 찜하기 관련 기능 구현

--- src/apis/deleteZim.ts
+++ src/apis/deleteZim.ts
@@ -5,18 +5,18 @@ import type { ZipkokResponse } from 'types/ZipkokResponse';
 /**
  * `DELETE /zim`으로 찜 취소를 요청합니다.
  */
-export async function deleteZim() {
+export async function deleteZim(realEstateId: number) {
   const path = '/zim';
   const method = 'DELETE';
-  const params = {};
+  const body = { realEstateId };
   const authRequired = true;
 
   const res = await api<ZipkokResponse<undefined>>(
     path,
     method,
     authRequired,
-    params,
     undefined,
+    body,
     undefined,
   );
 
--- src/pages/Main/My/LikedProperties/index.tsx
+++ src/pages/Main/My/LikedProperties/index.tsx
@@ -41,7 +41,7 @@ const LikedProperties = () => {
           propertyName={property.agent}
           imageUrl={property.imageURL}
           kokList={true}
-          onClick={() => navigate(`/kok/kokItem/${property.realEstateId}`)}
+          onClick={() => navigate(`/item/${property.realEstateId}`)}
         />
       ))}
     </div>
