aa8c14495c926a9fd47bae9984136173f50750dc
parkcoool <yickle1345@naver.com>
2024-02-20 04:22:30 +0900

    Fix: 콕리스트 수정에서 콕 옵션 데이터가 전송되도록 수정

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+import uncheckImg from 'assets/img/check/check_default.svg';
+import checkImg from 'assets/img/check/check_selected.svg';
 
 import styles from './Options.module.css';
-import checkImg from '../../assets/img/check/check_selected.svg';
 
 import type { UserKokOption } from 'apis/getUserKokOption';
 
@@ -9,37 +11,56 @@ interface OptionsComponentProps {
   kokOptions: UserKokOption[];
   setKokOptions?: React.Dispatch<React.SetStateAction<UserKokOption[]>>;
   readOnly?: boolean;
-  initialValue?: boolean;
 }
 
 const OptionsComponent: React.FC<OptionsComponentProps> = ({
   kokOptions,
   setKokOptions,
   readOnly,
-  initialValue,
 }) => {
   return (
     <div className={styles.root}>
-      {kokOptions.map((option, index) => (
+      {kokOptions.map((option) => (
         <div key={option.optionId}>
           <div className={styles.optionTitle}>
-            <img src={checkImg} />
+            <button
+              className="imgBtn"
+              onClick={() => {
+                if (readOnly) return;
+
+                setKokOptions?.((prevState) => {
+                  const newOptions = [...prevState];
+                  const index = newOptions.findIndex(
+                    (e) => e.optionId === option.optionId,
+                  );
+
+                  newOptions[index].isVisible = !option.isVisible;
+                  newOptions[index].detailOptions.forEach(
+                    (e) => (e.detailOptionIsVisible = option.isVisible),
+                  );
+
+                  return newOptions;
+                });
+              }}
+            >
+              <img src={option.isVisible ? checkImg : uncheckImg} />
+            </button>
             {option.optionTitle}
           </div>
 
           <div className={styles.checkCtn}>
-            {option.detailOptions.map((detailOption, detailIndex) => (
+            {option.detailOptions.map((detailOption) => (
               <div key={detailOption.detailOptionId}>
                 <input
                   type="checkbox"
                   className={styles.detailCheckBtn}
-                  checked={
-                    initialValue ? true : detailOption.detailOptionIsVisible
-                  }
+                  checked={detailOption.detailOptionIsVisible}
                   onChange={(e) => {
-                    if (readOnly) return false;
+                    if (readOnly) return;
+
                     setKokOptions?.((prevState) => {
                       const newOptions = [...prevState];
+
                       const parentIndex = newOptions.findIndex(
                         (e) => e.optionId === option.optionId,
                       );
@@ -48,9 +69,13 @@ const OptionsComponent: React.FC<OptionsComponentProps> = ({
                       ].detailOptions.findIndex(
                         (e) => e.detailOptionId === detailOption.detailOptionId,
                       );
-                      newOptions[index].detailOptions[
+
+                      newOptions[parentIndex].detailOptions[
                         detailIndex
                       ].detailOptionIsVisible = e.target.checked;
+                      newOptions[parentIndex].isVisible = newOptions[
+                        parentIndex
+                      ].detailOptions.some((e) => e.detailOptionIsVisible);
 
                       return newOptions;
                     });
--- src/components/Property/Contract/index.tsx
+++ src/components/Property/Contract/index.tsx
@@ -25,7 +25,6 @@ export default function Contract({
           kokOptions={options}
           setKokOptions={setOptions}
           readOnly={true}
-          initialValue={true}
         />
       </div>
 
